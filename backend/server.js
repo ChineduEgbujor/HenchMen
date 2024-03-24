dcbac8945ee726b094603dc21e4806840c02dc3a
ChineduEgbujor <112021948+ChineduEgbujor@users.noreply.github.com>
2024-03-24 11:27:06 -0700

    Delete backend directory

--- backend/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-app.get('/api/greeting', (req, res) => {
-    res.json({ message: 'Hello from the backend!' });
-});
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
